43b521e25697083a2f391db173bcadb1fb92c243
Aj-Ziad <ajdourziyad@gmail.com>
2026-01-03 13:21:04 +0100

    Convert frontend from submodule to normal folder

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Meals from './componentss/Meals/Meals';
+
+
+
+function App() {
+  return (
+   <>
+   <h1 className="text-3xl text-red-600 font-bold">Hhhh The best team in the entire Bootcamp</h1>
+  
+
+   </>
+  );
+}
+
+export default App;
